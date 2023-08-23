248cfd5b10603fbe757cc985aa55832cc008d65c
TheCreeep <thecreepfire@gmail.com>
2023-08-23 09:50:17 +0200

    added iframe

--- src/views/DataView.vue
+++ src/views/DataView.vue
@@ -1,8 +1,39 @@
-<script setup>
-</script>
-
 <template>
   <main>
-    DataView Yo
+      <iframe src="http://127.0.0.1:5000" scrolling="no" width="256" height="240"></iframe>
   </main>
 </template>
+
+<script>
+
+export default {
+  name: 'DataView',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+
+</script>
+
+<style lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  iframe {
+    transform: scale(2) translate(0, 100px);
+    border: 10px solid transparent;
+    border-image-source: url('../assets/brick.png');
+    border-image-slice: 100;
+  }
+}
+</style>
